26db6d70d979f744ec9aae3c18387c69bf9bbd06
Daniel <danielkong46@gmail.com>
2023-05-04 17:29:26 -0400

    added player ui, added transitions from questions to answers

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function showQuestion(){
     document.getElementById('board').style.display = 'none';
-    document.getElementById('question-screen').style.display = 'block';
+    document.getElementById('question-screen').style.display = 'flex';
+    let x = document.getElementsByClassName('buttons');
+    for (let i = 0; i < x.length; i++){
+        x[i].style.display = "block";
+    }
+}
 
+function showAnswer(){
+    document.getElementById('question-text').style.display = 'none';
+    document.getElementById('answer-text').style.display = 'block';
+    let x = document.getElementsByClassName('buttons');
+    for (let i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
 }
 
