b25c55d1a792599ef69f23058cdcf1f108394ce7
Daniel <danielkong46@gmail.com>
2023-05-12 21:36:08 -0400

    test game of offline jeopardy created

--- script.js
+++ script.js
@@ -21,15 +21,49 @@ var correctPlayer = -1;
 for (let i = 0; i < row; i++){
     questions[i] = [];
     answers[i] = [];
-    for (let i2 = 0; i2 < col; i2++){
-        questions[i][i2] = i + "-" + i2;
-        answers[i][i2] = "ans " + questions[i][i2];
-    }
 }
 
+for (let i = 0; i < col; i++){
+    questions[i][0] = "<img src='0-" + i + ".png'>";
+}
+answers[0][0]='Blastoise'; answers[1][0]='Sentret'; answers[2][0]='Dusclops'; answers[3][0]='Munna'; answers[4][0]='Drapion';
 
-questions[0][col] = "final jeopardy";
-answers[0][col] = "answer jeopardy"
+questions[0][1] = 'Top 10 Healthiest Foods'; questions[2][1]='Top 10 Songs that will make you cry'; questions[1][1]='Top 10 Most Intelligent Animals';questions[3][1]='Top 10 Insanely Racist Moments In Disney Movies That You Totally Forgot About';questions[4][1] = 'Top 5 Brutal Facts About Getting Shot'
+answers[0][1]='<ul><li>Avocado</li><li> Kale</li><li> Sweet Potato</li><li> Blueberries</li><li> Almond</li></ul><ul><li> Apple</li><li> Salmon</li><li> Oats</li><li> Garlic</li><li> Broccoli</li></ul>';
+answers[2][1]='<ul><li>Wild Horses</li><li> The Sound of Silence</li><li> Wish you were here</li><li> One sweet day</li><li> The living years</li></ul><ul><li> Candle in the wind</li><li> Hallelujah</li><li> Everybody Hurts</li><li> Hurt</li><li> Tears in Heaven</li></ul>';
+answers[1][1]='<ul><li>Raccoon</li><li>Border Collie</li><li>African Grey Parrot</li><li>Pig</li><li>Rat</li></ul><ul><li>Orangutan</li><li>Crows</li><li>Elephant</li><li>Bottlenose Dolphin</li><li>Chimpanzee</li></ul>'
+answers[3][1]="<ul><li>Fantasia</li><li>Commando Duck</li><li>Santa's Workshop</li><li>The Jungle Book</li><li>Aladdin</li></ul><ul><li>Peter Pan</li><li>Lady and the Tramp</li><li>The Aristocats</li><li>Dumbo</li><li>Song of the South</li>"
+answers[4][1]='<ul><li>Women are more likely to survive a gunshot</li><li>Tiny details make a big difference</li><li>Nanotechnology could have saved you</li><li>Getting shot will probably bankrupt you</li><li>There are worse things to get shot with than a gun</li></ul>'
+
+questions[0][2] = "A drawer contains black and white socks. What is the smallest number of socks that must be drawn from the drawer such that you are guaranteed to have a pair of similar colour socks?";
+questions[1][2]="<img src='2-2.jpg' class='question-img'> How many moves are required to transport the balls from the tube I to tube III if all the balls must remain in the same order?"
+questions[2][2]="<img src='2-3.jpg' class='question-img'>Peter's father has five sons. The names of four sons are Fefe, Fifi, Fafa and Fufu respectively. What is the name of the fifth son?";
+questions[3][2]="<img src='2-4.png' class='question-img'>How many triangles are in the picture?";
+questions[4][2]="<img src='2-5.jpg' class='question-img'>If you have a 7-minute hourglass and an 11-minute hourglass, how can you boil an egg in exactly 15 minutes?";
+answers[0][2] = "3";
+answers[1][2] = "5";
+answers[2][2]='Peter';
+answers[3][2]='16';
+answers[4][2] = "<ol style='font-size:30px'><li>Start both hourglasses as you start boiling the egg.</li><li>After the 7-minute hourglass runs out, turn it over to start it again.</li><li>Four minutes later, when the 11-minute hourglass runs out, turn the 7-minute hourglass again.</li><li>Wait for the 7-minute hourglass to run out, which will take another four minutes and get you to exactly 15 minutes of boiling time.</li></ol>";
+
+questions[0][3] = "A critically acclaimed dark fantasy series by Kentaro Miura";
+answers[0][3] = "Berserk";
+questions[1][3] = "Please name an anime that's animated by Madhouse";
+answers[1][3] = "Tatami Galaxy, Hxh, Chihayafuru, Ippo, No game no life, OPM, Death parade, Overlord, Parasyte, Trigun, Cardcaptor Sakura, Beyblade, Texhnolyze,  Black Lagoon, Death Note, Claymore, Btooom!, Irregular at magic highschool, Frieren, ...";
+questions[2][3] = "This anime's name translated means \"everyday life\" and is the highest rated slice of life anime on MAL";
+answers[2][3] = "Nichijou";
+questions[3][3] = "A holiday celebrated from April 29 to May 5 that is also the longest vacation period for Japanese salarymen"
+answers[3][3] = "Golden Week";
+questions[4][3] = "A song from this Japanese voice was recently heard in Guardians of the Galaxies Vol. 3";
+answers[4][3] = "Hatsune Miku (Would've accepted the name of any vocaloid)";
+
+for (let i = 0; i < row; i++){
+    questions[i][4] = "<video class='question-vid' controls autoplay> <source src='4-" + i + ".mp4' type='video/mp4'> </video>";
+}
+answers[0][4]='Brazil';answers[1][4]='China/Hong Kong idk';answers[2][4]='India';answers[3][4]='Russia';answers[4][4]='Italy';
+
+questions[0][col] = "<audio controls> <source src='final.mp3' type='audio/mp3'> </audio> What is the game?";
+answers[0][col] = "Parappa the Rapper 2";
 
 
 $(document).ready(function(){
@@ -159,14 +193,9 @@ function showWinScreen(){
     document.getElementById('win-screen').style.display = 'flex';
     document.getElementById('answer-screen').style.display = 'none';
     let x = document.getElementsByClassName('money');
-    let winnerMoney = parseInt(x[0].innerHTML);
-    let winnerIndex = 0;   
+    let winnerIndex = moneyAfterWagers.indexOf(Math.max.apply(null, moneyAfterWagers));   
     for (let i = 0; i < playerCount; i++){
         x[i].innerHTML = moneyAfterWagers[i] + "$";
-        if (winnerMoney < parseInt(x[i].innerHTML)){
-            winnerMoney = parseInt(x[i].innerHTML);
-            winnerIndex = i;
-        }
     }
 
     let winnerIcon = document.getElementsByClassName('player-icon')[winnerIndex].src;
@@ -240,7 +269,7 @@ function checkPowerCorrect(player){
         correctPlayer = player;
         let x = document.getElementsByClassName('board-cell');
         for (let i = 0; i < col; i++){
-            if (x[(currentQuestionRow+1)*5+i].nodeName == 'BUTTON' || x[(currentQuestionRow+1)*5+i].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7] ){
+            if (x[(currentQuestionRow+1)*5+i].nodeName == 'BUTTON' || x[(currentQuestionRow+1)*5+i].innerHTML == '' || x[(currentQuestionRow+1)*5+i].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7] ){
                 break;
             }
             if (i == col-1){
@@ -249,7 +278,7 @@ function checkPowerCorrect(player){
             }
         }
         for (let i = 1; i < row+1; i++){
-            if (x[i*5+currentQuestionCol].nodeName == 'BUTTON' || x[i*5+currentQuestionCol].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7]){
+            if (x[i*5+currentQuestionCol].nodeName == 'BUTTON' || x[i*5+currentQuestionCol].innerHTML == '' || x[i*5+currentQuestionCol].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7]){
                 break;
             }
             if (i == row){
@@ -259,7 +288,7 @@ function checkPowerCorrect(player){
         }
         if (currentQuestionCol == currentQuestionRow){
             for (let i = 0; i < row; i++){
-                if (x[5*(i+1) + i].nodeName == 'BUTTON' || x[5*(i+1) + i].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7]){
+                if (x[5*(i+1) + i].nodeName == 'BUTTON' || x[5*(i+1) + i].innerHTML == ''  || x[5*(i+1) + i].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7]){
                     console.log("i've broken" + (5*i) + " " + i);
                     break;
                 }
@@ -271,7 +300,7 @@ function checkPowerCorrect(player){
         }
         if (currentQuestionCol + currentQuestionRow == 4){
             for (let i = 1; i < row + 1; i++){
-                if (x[5*(i+1) - i].nodeName == 'BUTTON' || x[5*(i+1) - i].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7]){
+                if (x[5*(i+1) - i].nodeName == 'BUTTON' || x[5*(i+1) - i].innerHTML == '' || x[5*(i+1) - i].childNodes[0].src != "http://127.0.0.1:3000/" + characterIcons[7]){
                     console.log("i've broken" + (5*i) + " " + i);
                     break;
                 }
